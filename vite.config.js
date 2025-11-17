7eaca9d15aa4569302c2b1a5225cf4fcbdf78b35
Chaiyawit Suphannamok <lekchaiyawit@gmail.com>
2025-11-17 19:26:46 +0700

    new update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import moment from 'moment';
 import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 
